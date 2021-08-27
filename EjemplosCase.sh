e1d27779f64c6de115260aea454f2c0d45b73424
sancano22 <sandra.cano@gmail.com>
2021-08-27 12:02:12 -0400

    ejemplo de multiples casos y con  parámetros

---
+++ EjemplosCase.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+
+
+# MENU 1
+#echo "digite una opción..."
+#while : 
+#do
+#	read option
+#	case $option in
+#		hola) echo "Hola";;
+#		bye) echo "Hasta luego" ;;
+#		*) echo "Lo siento no es opción valida" ;;
+#    esac
+#done
+
+#echo "prueba"
+
+
+
+#MENU 2
+#while getopts ':p:m' option;
+#do
+#	case $option in
+#	   p) 
+#   			Nombre="$OPTARG"
+#   		
+#
+#			if [ "$Nombre" = "blocked" ];
+#	        then
+#	           echo "proceso bloqueado"
+#	        else 
+#	           echo "proceso"
+#	        fi;;
+#			
+#	   m) echo "memoria" ;;
+#
+#	   *) echo "opción invalida" 
+#		  exit 1 ;;
+#	esac
+#done
+
+
